287c16d57f051a7e6f32fb9171a6aa7315b993e4
draganov <draganov123@abv.bg>
2018-07-06 19:42:43 +0300

    modal scroll

--- js/recipe.js
+++ js/recipe.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var recipes = [];
 
     $.getJSON("https://api.myjson.com/bins/z3m2a")
-        .done(function(data){
-            $.each(data, function (index, value) {
+        .done(function(data) {
+            $.each(data, function(index, value) {
                 recipes.push(value);
             });
-    });
+        });
 
     var getById = function(id) {
-        let filteredItems = recipes.filter(a => {if (a.id === id) return a});
+        let filteredItems = recipes.filter(a => { if (a.id === id) return a });
 
         return filteredItems[0];
     }
@@ -22,23 +22,22 @@ $(document).ready(function(){
         var arrIngredients = item.ingredients.split(',');
         var arrInstructions = item.instructions.split(',');
 
-        for (let i = 0; i < 1; i++){
-          $('.modal .modal-body h2').text("");
-          for(let j = 0; j < arrIngredients.length - 1; j++){
-            $('.modal .modal-body h2').append('<li>' + arrIngredients[j] + '</li>');
-          }
-          $('.modal .modal-body h2').append('</br>' + item.instructions);
+        for (let i = 0; i < 1; i++) {
+            $('.modal .modal-body h2').text("");
+            for (let j = 0; j < arrIngredients.length - 1; j++) {
+                $('.modal .modal-body h2').append('<li>' + arrIngredients[j] + '</li>');
+            }
+            $('.modal .modal-body h2').append('</br>' + item.instructions);
         }
     }
 
-    $('.recipe-btn').on('click', function(){
+    $('.recipe-btn').on('click', function() {
         let id = $(this).attr('data-recipe-id');
         let item = getById(id);
         fillModal(item);
     });
 
-    $('.close-btn').on('click', function(){
+    $('.close-btn').on('click', function() {
         $('.modal').css('display', 'none');
     });
-
 });
